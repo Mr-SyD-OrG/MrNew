b98e85e209d376b787f4d8b2729f8ef953506b80
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-12 08:56:05 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,6 +1,6 @@
 # Don't Remove Credit @Syd_XyZ
 
-import re
+import re, time
 from os import environ
 from Script import script 
 
@@ -18,6 +18,7 @@ SESSION = environ.get('SESSION', 'TechVJBot')
 API_ID = int(environ.get('API_ID', '22182189'))
 API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
+UPTIME = time.time()
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
