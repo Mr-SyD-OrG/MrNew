b8a0363a8f50af59788d0359565bafd539d85d44
Mr-SyD-OrG <148736919+Mr-SyD-OrG@users.noreply.github.com>
2024-06-12 09:05:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,6 +19,8 @@ API_ID = int(environ.get('API_ID', '22182189'))
 API_HASH = environ.get('API_HASH', '5e7c4088f8e23d0ab61e29ae11960bf5')
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 UPTIME = time.time()
+WEBHOOK = bool(environ.get("WEBHOOK", True))
+
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 1800))
